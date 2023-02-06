563d77967d08de8a161d4774fa5bf15bd0817a5f
louis01056 <louis01056@gmail.com>
2023-02-06 15:43:46 -0800

    Spectrum1

--- sketch.js
+++ sketch.js
@@ -1,106 +1,42 @@
-var capture;
-var radius = 20;
-var imgCache 
-let overAllTexture
+
+let song, buttton, fft;
+
+function toggleSong() {
+  if(song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
+}
+
+function preload() {
+  song = loadSound('Ride On Time.mp3'); 
+}
 
 function setup() {
-	createCanvas(640,480);
-	capture = createCapture(VIDEO);
-	capture.size(640,480);
-	imgCache = createGraphics(640,480)
-	imgCache.translate(640,0)
-	imgCache.scale(-1,1)
-	// println(capture.height,capture.height)
-	// background(100);
-	rectMode(CENTER)
-	capture.hide()
-	
-	
-	overAllTexture=createGraphics(width,height)
-	overAllTexture.loadPixels()
-	// noStroke()
-	for(var i=0;i<width+50;i++){
-		for(var o=0;o<height+50;o++){
-			overAllTexture.set(i,o,color(100,noise(i/3,o/3,i*o/50)*random([0,50,100])))
-		}
-	}
-	overAllTexture.updatePixels()
-	
+  createCanvas(600, 600);
+  //angleMode(DEGREES); // Change the mode to DEGREES
+  button = createButton('Play');
+  button.mousePressed(toggleSong);
+  song.play();
+  fft = new p5.FFT(0.8, 512);
 }
-let mode = 1
-
 
 function draw() {
-	// loadImage(capture)
-	background(0);
-	imgCache.image(capture,0,0)
-	
-	push()
-		noStroke()
-		scale(1)
-		radius = max(mouseX,0)/10+20
-		for(var y=0;y<imgCache.height;y+=radius){
-			for(var x=0;x<imgCache.width;x+=radius){
-				var pixel = imgCache.get(x,y)
-				var r = pixel[0]
-				var g = pixel[1]
-				var b = pixel[2]
-
-				let bk = (r+g+b)/3
-				let bkI = 10-int(bk/25.5)
-				
-				if (mode==1){
-					let txt = "一二三天四五田電龍龕龘"
-					fill(r+50,g+50,b+50)
-					textSize(radius)
-					textStyle(BOLD)
-					text(txt[bkI],x,y)
-				}else if (mode==2){
-					ellipse(x,y,radius/3+b/15,radius/3+b/15)
-				}else if (mode==3){
-					push()
-						translate(x,y)
-						rotate(b/20)
-						colorMode(HSB)
-						fill(r,100,100)
-						// strokeWeight(3)
-						// noFill()
-						rect(0,0,radius/2.5+r/20,radius/2.5+r/20)
-						fill(0)
-						ellipse(0,0,5)
-					pop()
-				}
-				// fill(0)
-				// rect(x,y,radius/10+b/10-10,radius/10+b/10-20)
-			}	
-		}
-	pop()
-/*	
-	push()
-		blendMode(MULTIPLY)
-		image(overAllTexture,0,0)
-	pop()
-    */
-	
-	
-	// ellipse(mouseX, mouseY, 20, 20);
+  background(0);
+  
+  let spectrum = fft.analyze();
+  console.log(spectrum);
+  for (let i = 0; i < spectrum.length; i++) {
+    stroke(255);
+    let amp = spectrum[i];
+    let y = map(amp, 0, 256, height, 0);
+    line(i, height, i, y);
+  }
 }
 
-
-function keyPressed(){
-	if (key=="1"){
-		mode=1
-	}
-	if (key=="2"){
-		mode = 2
-	}
-	if (key=="3"){
-		mode = 3
-	}
-}
-function mousePressed(){
-	mode++
-	if (mode>3){
-		mode=1
-	}
+// Chrome 70 will require user gestures to enable web audio api
+// Click on the web page to start audio
+function touchStarted() {
+  getAudioContext().resume();
 }
