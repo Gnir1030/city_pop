8001d50dd14bdcecd9aae69bfc694f02c5cba070
louis01056 <louis01056@gmail.com>
2023-02-06 20:07:51 -0800

    Spectrum2

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
 
 let song, buttton, fft;
+let spectrum;
+let rate = 1;
+let oldmouseX, oldmouseY;
+let theta = 0;
 
 function toggleSong() {
   if(song.isPlaying()) {
@@ -17,36 +21,57 @@ function preload() {
 
 function setup() {
   createCanvas(1000, 600);
+  colorMode(HSB);
   angleMode(DEGREES); // Change the mode to DEGREES
   button = createButton('Pause');
   button.mousePressed(toggleSong);
   song.play();
-  fft = new p5.FFT(0.9, 1024);
+  fft = new p5.FFT(0.9, 2048);
 }
 
 function draw() {
   background(0);
+  song.rate(rate);
   translate(width/2, height/2);
   noFill();
   //stroke(color('red'));
-  let spectrum = fft.analyze();
+  spectrum = fft.analyze();
   let length = 512;
   radius = 100;
-  console.log(spectrum, spectrum.length);
-  //beginShape();
-  //vertex(0,height/2);
+  rotate(theta);
+  //console.log(spectrum, spectrum.length);
   for (let i = 0; i < 360; i++) {
-    stroke(color(i*255/360,255,255));
-    let amp = spectrum[i];
-    let y = map(amp, 0, length*3, 0, height);
-	let y2 = map(amp, 0, length, 0, height);
-	//vertex(i, y - height/2);
-	//vertex((y2 + radius) * cos(i), (y2 + radius)*sin(i));
-	//vertex(i, y2);
-	//vertex((radius - y) * cos(i), (radius - y)*sin(i));
+    stroke(color(i, 255,255));
+    let osc = spectrum[i];
+    let y = map(osc, 0, length*3, 0, height);
+	let y2 = map(osc, 0, length, 0, height);
 	line(radius * cos(i), radius * sin(i), (y2 + radius) * cos(i), (y2 + radius)*sin(i));
 	line(radius * cos(i), radius * sin(i), (radius - y) * cos(i), (radius - y)*sin(i));
   }
-  //endShape(CLOSE);
-  //ellipse(0,0, radius*2, radius*2); 
+}
+
+function mouseDragged(){
+	//console.log(spectrum);
+	if((mouseX - width/2) * (oldmouseY - height/2) - (mouseY - height/2) * (oldmouseX - width/2) < 0){ //vector product
+		theta++;
+		rate+=0.001;
+	}
+	else{
+		theta--;
+		rate-=0.001;
+	}
+
+	if(theta > 360){
+		theta = 0;
+	}
+	if(theta < 0){
+		theta = 360;
+	}
+
+	if(rate < 0.1){
+		rate = 0.1;
+	}
+
+	oldmouseX = mouseX;
+	oldmouseY = mouseY;
 }
