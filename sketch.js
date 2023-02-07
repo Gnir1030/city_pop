59e77f8741809acf1c12b3a7bbff7945bbe91277
louis01056 <louis01056@gmail.com>
2023-02-06 17:48:38 -0800

    Spectrum1-3

--- sketch.js
+++ sketch.js
@@ -16,32 +16,37 @@ function preload() {
 }
 
 function setup() {
-colorMode(HSB);
-  createCanvas(600, 600);
-  space_between_lines = width/128;
-  //angleMode(DEGREES); // Change the mode to DEGREES
+  createCanvas(1000, 600);
+  angleMode(DEGREES); // Change the mode to DEGREES
   button = createButton('Pause');
   button.mousePressed(toggleSong);
   song.play();
-  fft = new p5.FFT(0.9, 256);
+  fft = new p5.FFT(0.9, 1024);
 }
 
 function draw() {
   background(0);
+  translate(width/2, height/2);
+  noFill();
+  //stroke(color('red'));
   let spectrum = fft.analyze();
+  let length = 512;
+  radius = 100;
   console.log(spectrum, spectrum.length);
-  for (let i = 0; i < spectrum.length; i++) {
-    //stroke(random(0,255),random(0,255),random(0,255));
+  //beginShape();
+  //vertex(0,height/2);
+  for (let i = 0; i < 360; i++) {
+    stroke(color(i*255/360,255,255));
     let amp = spectrum[i];
-    let y = map(amp, 0, 1024, height, 0);
-	fill(i,255,255);
-	rect(i * space_between_lines, height/2, space_between_lines, height - y);
-	rect(i * space_between_lines, y - height/2 , space_between_lines, height - y);
+    let y = map(amp, 0, length*3, 0, height);
+	let y2 = map(amp, 0, length, 0, height);
+	//vertex(i, y - height/2);
+	//vertex((y2 + radius) * cos(i), (y2 + radius)*sin(i));
+	//vertex(i, y2);
+	//vertex((radius - y) * cos(i), (radius - y)*sin(i));
+	line(radius * cos(i), radius * sin(i), (y2 + radius) * cos(i), (y2 + radius)*sin(i));
+	line(radius * cos(i), radius * sin(i), (radius - y) * cos(i), (radius - y)*sin(i));
   }
-}
-
-// Chrome 70 will require user gestures to enable web audio api
-// Click on the web page to start audio
-function touchStarted() {
-  getAudioContext().resume();
+  //endShape(CLOSE);
+  //ellipse(0,0, radius*2, radius*2); 
 }
