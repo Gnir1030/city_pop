b64554fd74dbe26d9b9681b5611ee520078812ea
louis01056 <louis01056@gmail.com>
2023-02-06 22:07:26 -0800

    spectrum4

--- sketch.js
+++ sketch.js
@@ -22,11 +22,12 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1000, 600);
+  let cnv = createCanvas(1000, 600);
   colorMode(HSB, 360, 100, 100, 100);
   angleMode(DEGREES); // Change the mode to DEGREES
   button = createButton('Pause');
   button.mousePressed(toggleSong);
+  cnv.mouseWheel(changeVib);
   song.play();
   fft = new p5.FFT(0.9, 2048);
 }
@@ -36,12 +37,9 @@ function draw() {
   song.rate(rate);
   translate(width/2, height/2);
   noFill();
-  //stroke(color('red'));
   spectrum = fft.analyze();
-  //length = 1024;
   radius = 100;
   rotate(theta);
-  //console.log(spectrum, spectrum.length);
   for (let i = 0; i < 360; i+=vib) {
     stroke(color(i, 255,255));
     let osc = spectrum[i];
@@ -50,8 +48,6 @@ function draw() {
 	drawingContext.shadowBlur = 32;
 	drawingContext.shadowColor = color(207,7,99);
 	line((radius - y) * cos(i), (radius - y)*sin(i), (y2 + radius) * cos(i), (y2 + radius)*sin(i));
-	//line(radius * cos(i), radius * sin(i), (radius - y) * cos(i), (radius - y)*sin(i));
-	//vertex()
   }
 }
 
@@ -67,14 +63,12 @@ function mouseDragged(){
 		//rate-=0.001;
 		length+=10;
 	}
-
 	if(theta > 360){
 		theta = 0;
 	}
 	if(theta < 0){
 		theta = 360;
 	}
-
 	if(rate < 0.1){
 		rate = 0.1;
 	}
@@ -83,6 +77,17 @@ function mouseDragged(){
 	oldmouseY = mouseY;
 }
 
-function mouseWheel(event) {
-	vib++;
-  }
+function changeVib(event) {
+	if (event.deltaY > 0) {
+		vib++;
+	  }
+	else {
+		vib--;
+	}
+	if(vib == 0){
+		vib = 1;
+	}
+}
+
+
+  
